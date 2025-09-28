897f84be4c429e24dd5dcbf4dc641388e537ece7
Cleber-0101 <cleberbatista99@hotmail.com>
2025-09-28 13:41:52 -0300

    Variaveis em Python e convenção de codigo

---
+++ fundamentos.py
@@ -0,0 +1,5 @@
+# 
+
+#duas formas de importar arquivos
+# import tipos.variaveis
+from tipos import variaveis
---
+++ tipos/variaveis.py
@@ -0,0 +1,24 @@
+# #Não existe tipagem no Python 
+
+# a = 4
+# b = 4
+# z = a + b
+
+# print(z)
+
+# #string
+# text = "Meu nome é Cleber e tenho " 
+# idade = 25 
+
+# print(text + str( idade) + " anos de idade")
+
+# # F-string
+# print(f'{text}')
+
+
+# Convenção para Escrever uma constante em python 
+PI = 3.14
+raio = float(input("Escreva o valor da circuferencia : "))
+area = PI * pow(raio, 2)
+
+print(f'O valor de area é de {area} m2')
